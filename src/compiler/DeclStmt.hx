78d7cf75c1425f47d99ee748432e916792ef3cbe
ALANVF <alan.invents@gmail.com>
2021-04-11 16:26:29 -0400

    Actually done with types now

--- src/compiler/DeclStmt.hx
+++ src/compiler/DeclStmt.hx
@@ -2,6 +2,8 @@ package compiler;
 
 enum DeclStmt {
 	DTypeDecl(decl: TypeDecl);
+	DAnonEnum(cases: EnumCases);
+	DAnonUnion(body: DeclBody);
 	DNamespace(ns: Namespace);
 	DUsing(kind: Option<String>, type: Type);
 	DUsingExpr(expr: Expr);
